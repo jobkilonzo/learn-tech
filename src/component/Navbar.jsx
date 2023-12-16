18a7c80b31fab39d0515b5f6043b180efef4dda2
jobkilonzo <jobkilonzo95@gmail.com>
2023-12-16 14:40:45 +0300

    Edited navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -21,18 +21,18 @@ const Navbar = () => {
         </ul>
         <div onClick={handleNab} className='block md:hidden'>
           
-          {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
           
         </div> 
         <div className={nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out' : 'fixed left-[-100%]'}>
         
-        <h1 className='w-full hover:text-white text-3xl font-bold text-[#00df9a] m-4'>Learn Tech</h1>
+        <h1 className='w-full md:hidden hover:text-white text-3xl font-bold px-4 text-[#00df9a] mt-1'>Learn Tech</h1>
         <ul className='p-4 uppercase'>
             <li className='p-4 border-b border-gray-600 hover:text-[rgb(91,201,166)]'>Home</li>
             <li className='p-4 border-b border-gray-600 hover:text-[rgb(91,201,166)]'><a href="#message">Message From Director</a>r</li>
             <li className='p-4 border-b border-gray-600 hover:text-[rgb(91,201,166)]'><a href="#newsletter">Newsletter</a>r</li>
             <li className='p-4 hover:text-[rgb(91,201,166)]'><a href="#packages">Packages</a></li>
-        </ul>
+        </ul> 
         </div>
     </div>
   )
